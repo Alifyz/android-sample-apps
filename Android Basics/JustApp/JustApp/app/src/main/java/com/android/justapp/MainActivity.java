4115e38cee7b4f865815d90468500611a558909f
Alifyz Pires <alifyz@outlook.com>
2017-06-28 22:33:30 -0300

    Playing with Variables

--- Android Basics/JustApp/JustApp/app/src/main/java/com/android/justapp/MainActivity.java
+++ Android Basics/JustApp/JustApp/app/src/main/java/com/android/justapp/MainActivity.java
@@ -1,7 +1,12 @@
 package com.android.justapp;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
+
+import java.text.NumberFormat;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+
+    public void submitOrder(View view)
+    {
+        int numberofCoffes = 2;
+        display(numberofCoffes);
+        displayPrice(numberofCoffes * 5);
+    }
+
+    private void display(int number) {
+        TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
+        quantityTextView.setText("" + number);
+    }
+
+    /**
+     * This method displays the given price on the screen.
+     */
+    private void displayPrice(int number) {
+        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
+        priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
     }
 }
