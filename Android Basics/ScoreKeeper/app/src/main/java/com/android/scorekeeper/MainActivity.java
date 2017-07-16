9ceb8550509f4488e57ae426e27e44ce74176d1f
Alifyz <alifyz@outlook.com>
2017-07-15 23:56:31 -0300

    Implementing SharePreferences - Udacity ScoreKeeper App

--- Android Basics/ScoreKeeper/app/src/main/java/com/android/scorekeeper/MainActivity.java
+++ Android Basics/ScoreKeeper/app/src/main/java/com/android/scorekeeper/MainActivity.java
@@ -16,10 +16,53 @@ public class MainActivity extends AppCompatActivity {
     private int faltasA = 0;
     private int faltasB = 0;
 
+
+    private static final String PONTOS_A = "TIME_A";
+    private static final String PONTOS_B = "TIME_B";
+    private static final String FALTAS_A = "FALTAS_A";
+    private static final String FALTAS_B = "FALTAS_B";
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // OBRIGADO dhiegoabrantes, por me mostrar o SharePreferences
+        // Código Implementado de Acordo com os Padrões no Link abaixo
+        // https://developer.android.com/training/basics/activity-lifecycle/recreating.html?hl=pt-br#RestoreState
+
+        // Check whether we're recreating a previously destroyed instance
+        if (savedInstanceState != null) {
+            // Restore value of members from saved state
+            pointsTeamA = savedInstanceState.getInt(PONTOS_A);
+            pointsTeamB = savedInstanceState.getInt(PONTOS_B);
+            faltasA = savedInstanceState.getInt(FALTAS_A);
+            faltasB = savedInstanceState.getInt(FALTAS_B);
+            displayA(pointsTeamA);
+            displayB(pointsTeamB);
+            setFaltasA(faltasA);
+            setFaltasB(faltasB);
+
+        } else {
+            pointsTeamA = 0;
+            pointsTeamB = 0;
+            faltasA = 0;
+            faltasB = 0;
+        }
+
+    }
+
+
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+        // Save the user's current game state
+        savedInstanceState.putInt(PONTOS_A, pointsTeamA);
+        savedInstanceState.putInt(PONTOS_B, pointsTeamB);
+        savedInstanceState.putInt(FALTAS_A, faltasA);
+        savedInstanceState.putInt(FALTAS_B, faltasB);
+        // Always call the superclass so it can save the view hierarchy state
+        super.onSaveInstanceState(savedInstanceState);
     }
 
     /*
@@ -59,7 +102,6 @@ public class MainActivity extends AppCompatActivity {
         setFaltasB(faltasB);
     }
 
-
     /*
      * Display Methods - Alterando os Valores das Views
      */
@@ -83,8 +125,6 @@ public class MainActivity extends AppCompatActivity {
         TextView quantityTextView = (TextView) findViewById(R.id.faltas_tb);
         quantityTextView.setText("" + number);
     }
-
-
 
 
 }
