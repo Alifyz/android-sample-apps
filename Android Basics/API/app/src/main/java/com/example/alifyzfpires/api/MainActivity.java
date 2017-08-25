f43e778db272a3932dd622506642fb8d3862ae2e
alifyz <alifyz@outlook.com>
2017-08-25 17:55:52 -0300

    Logic and + Coding Questions

--- Android Basics/API/app/src/main/java/com/example/alifyzfpires/api/MainActivity.java
+++ Android Basics/API/app/src/main/java/com/example/alifyzfpires/api/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.alifyzfpires.api;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.RadioGroup;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,10 +12,13 @@ public class MainActivity extends AppCompatActivity {
     int moderado = 0;
     int agressivo = 0;
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Spinner ageSpinner = (Spinner) findViewById(R.id.age);
+        RadioGroup firstQuestion = (RadioGroup) findViewById(R.id.q1);
+
     }
 }
