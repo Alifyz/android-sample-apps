f16dbe7cf0e065470901b1c046fac5283e218f22
Alifyz Pires <alifyz@outlook.com>
2017-08-26 00:51:21 -0300

    Working on the Quiz App Logic

--- Android Basics/API/app/src/main/java/com/example/alifyzfpires/api/MainActivity.java
+++ Android Basics/API/app/src/main/java/com/example/alifyzfpires/api/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.alifyzfpires.api;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.RadioGroup;
 import android.widget.Spinner;
 
@@ -12,13 +14,28 @@ public class MainActivity extends AppCompatActivity {
     int moderado = 0;
     int agressivo = 0;
 
+    Spinner ageSpinner;
+    RadioGroup firstQuestion;
+    RadioGroup secondQuestion;
+    RadioGroup thirdQuestion;
+    RadioGroup fourthQuestion;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Spinner ageSpinner = (Spinner) findViewById(R.id.age);
-        RadioGroup firstQuestion = (RadioGroup) findViewById(R.id.q1);
+        ageSpinner = (Spinner) findViewById(R.id.age);
+        firstQuestion = (RadioGroup) findViewById(R.id.q1);
+        secondQuestion = (RadioGroup) findViewById(R.id.q2);
+        thirdQuestion = (RadioGroup) findViewById(R.id.q3);
+        fourthQuestion = (RadioGroup) findViewById(R.id.q4);
+
+
+
+    }
+
+    public void evaluateProfile(View v) {
 
     }
 }
