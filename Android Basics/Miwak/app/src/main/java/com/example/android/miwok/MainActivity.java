767582a875e11463b26ce48657edccf4dcafa9d8
alifyz <alifyz@outlook.com>
2017-09-13 23:30:23 -0300

    Removing onClick Events from XML Layout and addiging eventListeners in Java Code.

--- Android Basics/Miwak/app/src/main/java/com/example/android/miwok/MainActivity.java
+++ Android Basics/Miwak/app/src/main/java/com/example/android/miwok/MainActivity.java
@@ -19,35 +19,53 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        // Set the content of the activity to use the activity_main.xml layout file
         setContentView(R.layout.activity_main);
 
+        TextView numbers = (TextView) findViewById(R.id.numbers);
+        TextView family =  (TextView) findViewById(R.id.family);
+        TextView colors =  (TextView) findViewById(R.id.colors);
+        TextView phrases = (TextView) findViewById(R.id.phrases);
+
+        numbers.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent openNumbers = new Intent(MainActivity.this, NumbersActivity.class);
+                startActivity(openNumbers);
+            }
+        });
+
+        family.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent openFamily = new Intent(MainActivity.this, FamilyMembersActivity.class);
+                startActivity(openFamily);
+            }
+        });
+
+        colors.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent openColors = new Intent(MainActivity.this, ColorsActivity.class);
+                startActivity(openColors);
+            }
+        });
+
+        phrases.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent openPhrases = new Intent(MainActivity.this, PhrasesActivity.class);
+                startActivity(openPhrases);
+            }
+        });
+
     }
 
-    public void OpenNumbers(View view) {
-        Intent openNumbers = new Intent(this, NumbersActivity.class);
-        startActivity(openNumbers);
-    }
 
-    public void OpenFamily(View view) {
-        Intent openFamily = new Intent(this, FamilyMembersActivity.class);
-        startActivity(openFamily);
-    }
-
-    public void OpenColors(View view) {
-        Intent openColors = new Intent(this, ColorsActivity.class);
-        startActivity(openColors);
-    }
-
-    public void OpenPhrases(View view) {
-        Intent openPhrases = new Intent(this, PhrasesActivity.class);
-        startActivity(openPhrases);
-    }
 }
