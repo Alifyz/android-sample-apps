9eea99af18f4bcfa52d47639f59cbae7885a5804
alifyz <alifyz@outlook.com>
2017-09-17 22:48:57 -0300

    -Adding EventListeners -Adding 3 more Activities

--- Android Basics/MusicStation/app/src/main/java/com/example/alifyzfpires/musicstation/MainActivity.java
+++ Android Basics/MusicStation/app/src/main/java/com/example/alifyzfpires/musicstation/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.alifyzfpires.musicstation;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,44 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView reproduzir = (TextView) findViewById(R.id.reproduzir);
+        TextView playlist   = (TextView) findViewById(R.id.playlist);
+        TextView premium    = (TextView) findViewById(R.id.premium);
+        TextView config     = (TextView) findViewById(R.id.configurações);
+
+        reproduzir.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent reproduzir = new Intent(MainActivity.this, Reproduzir.class);
+                startActivity(reproduzir);
+            }
+        });
+
+        playlist.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent playlist = new Intent(MainActivity.this, Playlist.class);
+                startActivity(playlist);
+            }
+        });
+
+        premium.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent premium = new Intent(MainActivity.this, com.example.alifyzfpires.musicstation.premium.class);
+                startActivity(premium);
+            }
+        });
+
+        config.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent configuracoes = new Intent(MainActivity.this, com.example.alifyzfpires.musicstation.configuracoes.class);
+                startActivity(configuracoes);
+            }
+        });
+
+
     }
 }
---
+++ Android Basics/MusicStation/app/src/main/java/com/example/alifyzfpires/musicstation/configuracoes.java
@@ -0,0 +1,13 @@
+package com.example.alifyzfpires.musicstation;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class configuracoes extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_configuracoes);
+    }
+}
