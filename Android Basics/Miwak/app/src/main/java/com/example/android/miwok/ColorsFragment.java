cf555cd6b1acb4001a325704af59ed39492ad778
alifyz <alifyz@outlook.com>
2017-10-23 18:24:38 -0200

    Code Refactoring and Implementing Fragments

---
+++ Android Basics/Miwak/app/src/main/java/com/example/android/miwok/ColorsFragment.java
@@ -0,0 +1,36 @@
+package com.example.android.miwok;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class ColorsFragment extends Fragment {
+
+
+    public ColorsFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        View rootView = inflater.inflate(R.layout.activity_colors, container, false);
+
+
+
+
+
+
+        return rootView;
+
+    }
+
+}
