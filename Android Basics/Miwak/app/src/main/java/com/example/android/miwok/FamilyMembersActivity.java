10f1a3c38e276ad98e672379bba054e3bf3cb68d
alifyz <alifyz@outlook.com>
2017-10-23 18:58:04 -0200

    Implementing Family Members Fragments

--- Android Basics/Miwak/app/src/main/java/com/example/android/miwok/FamilyMembersActivity.java
+++ Android Basics/Miwak/app/src/main/java/com/example/android/miwok/FamilyMembersActivity.java
@@ -1,81 +1,19 @@
 package com.example.android.miwok;
 
-import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ListView;
-
-import java.util.ArrayList;
 
 public class FamilyMembersActivity extends AppCompatActivity {
 
-    MediaPlayer audioPlayer;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_family_members);
-
-
-        final ArrayList<Word> words = new ArrayList<Word>();
-
-        words.add(new Word("father","әpә", R.drawable.family_father, R.raw.family_father));
-        words.add(new Word("mother","әṭa", R.drawable.family_mother, R.raw.family_mother));
-        words.add(new Word("son","angsi", R.drawable.family_son, R.raw.family_son));
-        words.add(new Word("daughter","tune", R.drawable.family_daughter, R.raw.family_daughter));
-        words.add(new Word("older brother","taachi" , R.drawable.family_older_brother, R.raw.family_older_brother));
-        words.add(new Word("younger brother","chalitti", R.drawable.family_younger_brother, R.raw.family_younger_brother));
-        words.add(new Word("older sister","\t\n" + "teṭe", R.drawable.family_older_sister, R.raw.family_older_sister));
-        words.add(new Word("younger sister","kolliti", R.drawable.family_younger_sister,R.raw.family_younger_sister));
-        words.add(new Word("grandmother","ama", R.drawable.family_grandmother, R.raw.family_grandmother));
-        words.add(new Word("grandfather","paapa", R.drawable.family_grandfather, R.raw.family_grandfather));
-
-        WordAdapter wordAdapter = new WordAdapter(this, words, R.color.category_family);
-
-        ListView listView = (ListView) findViewById(R.id.family);
-        listView.setAdapter(wordAdapter);
-
-
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                releaseMediaPlayer();
-                audioPlayer = MediaPlayer.create(FamilyMembersActivity.this, words.get(i).getResourceAudioId());
-                audioPlayer.start();
-
-                audioPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
-                    @Override
-                    public void onCompletion(MediaPlayer mediaPlayer) {
-                        releaseMediaPlayer();
-                    }
-                });
-            }
-        });
+        setContentView(R.layout.container);
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.container, new FamilyMembersFragment())
+                .commit();
     }
 
-    @Override
-    protected void onStop() {
-        super.onStop();
-        releaseMediaPlayer();
-    }
-
-
-    /**
-     * Clean up the media player by releasing its resources.
-     */
-    private void releaseMediaPlayer() {
-        // If the media player is not null, then it may be currently playing a sound.
-        if (audioPlayer != null) {
-            // Regardless of the current state of the media player, release its resources
-            // because we no longer need it.
-            audioPlayer.release();
-
-            // Set the media player back to null. For our code, we've decided that
-            // setting the media player to null is an easy way to tell that the media player
-            // is not configured to play an audio file at the moment.
-            audioPlayer = null;
-        }
-    }
 }
