6a1f1e7e361a5cd8374bfe9e8385243b0c7f98b8
alifyz <alifyz@outlook.com>
2017-10-25 00:27:00 -0200

    Adding - 4  Fragments for the Activities and List View Container.

---
+++ Android Basics/CanadaTour/app/src/main/java/com/alifyz/canadatour/MuseaumFragment.java
@@ -0,0 +1,30 @@
+package com.alifyz.canadatour;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class MuseaumFragment extends Fragment {
+
+
+    public MuseaumFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        TextView textView = new TextView(getActivity());
+        textView.setText(R.string.hello_blank_fragment);
+        return textView;
+    }
+
+}
