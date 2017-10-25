de74cda83000307ee12f4951e7eedfa536bf8436
alifyz <alifyz@outlook.com>
2017-10-25 00:35:26 -0200

    Extending the ArrayAdapter to support the ListView features and Inflate the ListView Layout

--- Android Basics/CanadaTour/app/src/main/java/com/alifyz/canadatour/PlaceAdapter.java
+++ Android Basics/CanadaTour/app/src/main/java/com/alifyz/canadatour/PlaceAdapter.java
@@ -1,12 +1,12 @@
 package com.alifyz.canadatour;
 
 import android.content.Context;
-import android.support.annotation.LayoutRes;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+
+import java.util.ArrayList;
 
 /**
  * Created by Alifyz F Pires on 10/25/2017.
@@ -15,13 +15,24 @@ import android.widget.ArrayAdapter;
 
 public class PlaceAdapter extends ArrayAdapter<Place> {
 
-    public PlaceAdapter(@NonNull Context context, @LayoutRes int resource, @NonNull Place[] objects) {
-        super(context, resource, objects);
+
+    public PlaceAdapter(Context context, ArrayList<Place> places) {
+        super(context, 0, places);
     }
 
-    @NonNull
+
     @Override
-    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
-        return super.getView(position, convertView, parent);
+    public View getView(int position, View convertView, ViewGroup parent) {
+
+        View listItemView = convertView;
+        if(listItemView == null) {
+            listItemView = LayoutInflater.from(getContext()).inflate(
+                    R.layout.listview_container, parent, false);
+        }
+
+
+
+        return listItemView;
+
     }
 }
