5e6ccbf2a6cd07795adc463b3a0e5dbbb94ecbab
Alifyz <alifyz@outlook.com>
2017-10-28 01:38:54 -0200

    Creating the Nature Activity

--- Android Basics/CanadaTour/app/src/main/java/com/alifyz/canadatour/Nature.java
+++ Android Basics/CanadaTour/app/src/main/java/com/alifyz/canadatour/Nature.java
@@ -1,7 +1,10 @@
 package com.alifyz.canadatour;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class Nature extends AppCompatActivity {
 
@@ -9,5 +12,20 @@ public class Nature extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_nature);
+
+        ArrayList<Place> nature = new ArrayList<Place>();
+
+
+        nature.add(new Place(R.string.firstNatureTitle,R.string.firstNatureDescription,
+                R.drawable.alberta));
+
+
+        PlaceAdapter adapter = new PlaceAdapter(this, nature);
+
+        ListView listView = (ListView) findViewById(R.id.nature);
+        listView.setAdapter(adapter);
+
+
+
     }
 }
