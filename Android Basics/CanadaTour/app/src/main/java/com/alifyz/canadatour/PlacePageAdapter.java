ee1ce7dfd07642bb10fef07feee7a8808a9316b6
Alifyz <alifyz@outlook.com>
2017-10-28 01:59:58 -0200

    Implementing the Custom PageAdapter for Fragments

---
+++ Android Basics/CanadaTour/app/src/main/java/com/alifyz/canadatour/PlacePageAdapter.java
@@ -0,0 +1,38 @@
+package com.alifyz.canadatour;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+/**
+ * Created by Alifyz on 10/28/2017.
+ * Custom PageAdapter to Implement the ViewPager
+ */
+
+public class PlacePageAdapter extends FragmentPagerAdapter {
+
+    public PlacePageAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        switch (position) {
+            case 0:
+                return new MainFragment();
+            case 1:
+                return new NatureFragment();
+            case 2:
+                return new RestaurantsFragment();
+            case 3:
+                return new SportFragment();
+            default:
+                return null;
+        }
+    }
+
+    @Override
+    public int getCount() {
+        return 4;
+    }
+}
