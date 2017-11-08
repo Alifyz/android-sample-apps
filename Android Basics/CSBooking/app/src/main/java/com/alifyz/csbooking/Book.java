06338a16cd8d4391938bda5e85ac8c0f3d2e3912
Alifyz <alifyz@outlook.com>
2017-11-07 22:09:28 -0200

    Add - Custom Book Objects

---
+++ Android Basics/CSBooking/app/src/main/java/com/alifyz/csbooking/Book.java
@@ -0,0 +1,38 @@
+package com.alifyz.csbooking;
+
+import android.graphics.Bitmap;
+
+/**
+ * Created by Alifyz on 11/7/2017.
+ */
+
+public class Book {
+
+    private Bitmap mImageResource;
+    private String mTitle;
+    private String mAuthor;
+    private String mDescripton;
+
+    public Book(Bitmap mImageResource, String mTitle, String mAuthor, String mDescripton) {
+        this.mImageResource = mImageResource;
+        this.mTitle = mTitle;
+        this.mAuthor = mAuthor;
+        this.mDescripton = mDescripton;
+    }
+
+    public Bitmap getmImageResource() {
+        return mImageResource;
+    }
+
+    public String getmTitle() {
+        return mTitle;
+    }
+
+    public String getmAuthor() {
+        return mAuthor;
+    }
+
+    public String getmDescripton() {
+        return mDescripton;
+    }
+}
