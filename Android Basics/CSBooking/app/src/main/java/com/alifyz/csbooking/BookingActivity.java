3794fd62e9f2af612c7882255427e999eb32b467
Alifyz <alifyz@outlook.com>
2017-11-08 00:02:27 -0200

    Add - Custom BookUtils Object with Helper methods to make HTTPRequest and extract the rawJSON and the InputStream.

--- Android Basics/CSBooking/app/src/main/java/com/alifyz/csbooking/BookingActivity.java
+++ Android Basics/CSBooking/app/src/main/java/com/alifyz/csbooking/BookingActivity.java
@@ -2,12 +2,17 @@ package com.alifyz.csbooking;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.SearchView;
 
 public class BookingActivity extends AppCompatActivity {
+
+    private SearchView searchBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home_template);
+
+        searchBar = (SearchView) findViewById(R.id.search_view);
     }
 }
