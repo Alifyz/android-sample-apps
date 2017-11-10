23f6906d4d0e1df4088395dece81ad6ddd717c09
Alifyz <alifyz@outlook.com>
2017-11-10 21:06:35 -0200

    - Prototyping the Layout Home Screen.

---
+++ Android Basics/NewsApp/app/src/main/java/com/alifyz/newsapp/NewsHome.java
@@ -0,0 +1,13 @@
+package com.alifyz.newsapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NewsHome extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.news_activity);
+    }
+}
