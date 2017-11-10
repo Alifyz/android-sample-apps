9904b137a30477476ccc0fb46e7d62cc47e3ab80
Alifyz <alifyz@outlook.com>
2017-11-10 20:30:44 -0200

    - Changing the Styles and the App feels

--- Android Basics/NewsApp/app/src/main/java/com/alifyz/newsapp/NewsActivity.java
+++ Android Basics/NewsApp/app/src/main/java/com/alifyz/newsapp/NewsActivity.java
@@ -3,11 +3,11 @@ package com.alifyz.newsapp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class NewsActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.news_activity);
     }
 }
