92d978c494b654c2349e80afe1cadddc5c7145de
Alifyz <alifyz@outlook.com>
2017-11-10 22:08:37 -0200

    - Linking the home screen to the News Main Activity

--- Android Basics/NewsApp/app/src/main/java/com/alifyz/newsapp/NewsActivity.java
+++ Android Basics/NewsApp/app/src/main/java/com/alifyz/newsapp/NewsActivity.java
@@ -8,6 +8,6 @@ public class NewsActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.news_activity);
+        setContentView(R.layout.list_view);
     }
 }
--- Android Basics/NewsApp/app/src/main/java/com/alifyz/newsapp/NewsHome.java
+++ Android Basics/NewsApp/app/src/main/java/com/alifyz/newsapp/NewsHome.java
@@ -1,13 +1,28 @@
 package com.alifyz.newsapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class NewsHome extends AppCompatActivity {
+
+    private Button btnLoad;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.news_activity);
+
+        btnLoad = (Button) findViewById(R.id.button);
+        btnLoad.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent loadNewsActivity = new Intent(NewsHome.this, NewsActivity.class);
+                startActivity(loadNewsActivity);
+            }
+        });
+
     }
 }
