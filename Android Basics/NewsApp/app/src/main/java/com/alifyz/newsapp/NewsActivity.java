ed9e7def79b7aec228b36da22e1915edaad6de3f
Alifyz <alifyz@outlook.com>
2017-11-10 23:08:47 -0200

    - Adding the Custom AsyncTaskLoader - Adding the Interface Callbacks

--- Android Basics/NewsApp/app/src/main/java/com/alifyz/newsapp/NewsActivity.java
+++ Android Basics/NewsApp/app/src/main/java/com/alifyz/newsapp/NewsActivity.java
@@ -1,13 +1,31 @@
 package com.alifyz.newsapp;
 
+import android.app.LoaderManager;
+import android.content.Loader;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class NewsActivity extends AppCompatActivity {
+public class NewsActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<News>{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.list_view);
+    }
+
+
+    @Override
+    public Loader<News> onCreateLoader(int i, Bundle bundle) {
+        return null;
+    }
+
+    @Override
+    public void onLoadFinished(Loader<News> loader, News news) {
+
+    }
+
+    @Override
+    public void onLoaderReset(Loader<News> loader) {
+
     }
 }
