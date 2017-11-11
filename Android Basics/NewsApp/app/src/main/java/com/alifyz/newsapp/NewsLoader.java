00203bcc5c670f121edb1d3d820ee86fed46cb7e
Alifyz <alifyz@outlook.com>
2017-11-10 22:15:10 -0200

    - Adding the Custom AsyncTaskLoader

--- Android Basics/NewsApp/app/src/main/java/com/alifyz/newsapp/NewsLoader.java
+++ Android Basics/NewsApp/app/src/main/java/com/alifyz/newsapp/NewsLoader.java
@@ -1,8 +1,20 @@
 package com.alifyz.newsapp;
 
+import android.content.AsyncTaskLoader;
+import android.content.Context;
+
 /**
  * Created by Alifyz on 11/10/2017.
  */
 
-public class NewsLoader {
+public class NewsLoader extends AsyncTaskLoader<News> {
+
+    public NewsLoader(Context context) {
+        super(context);
+    }
+
+    @Override
+    public News loadInBackground() {
+        return null;
+    }
 }
