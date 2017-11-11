4435ac5a3bd33927b5cfb1a49c662aff19d4650a
Alifyz <alifyz@outlook.com>
2017-11-10 22:11:33 -0200

    - Adding the News Object to the Project.

--- Android Basics/NewsApp/app/src/main/java/com/alifyz/newsapp/News.java
+++ Android Basics/NewsApp/app/src/main/java/com/alifyz/newsapp/News.java
@@ -5,4 +5,38 @@ package com.alifyz.newsapp;
  */
 
 public class News {
+
+    private String mTitle;
+    private String mDescription;
+    private String mDatePublication;
+    private String mAuthor;
+    private String mCategory;
+
+    public News(String mTitle, String mDescription, String mDatePublication, String mAuthor, String mCategory) {
+        this.mTitle = mTitle;
+        this.mDescription = mDescription;
+        this.mDatePublication = mDatePublication;
+        this.mAuthor = mAuthor;
+        this.mCategory = mCategory;
+    }
+
+    public String getmTitle() {
+        return mTitle;
+    }
+
+    public String getmDescription() {
+        return mDescription;
+    }
+
+    public String getmDatePublication() {
+        return mDatePublication;
+    }
+
+    public String getmAuthor() {
+        return mAuthor;
+    }
+
+    public String getmCategory() {
+        return mCategory;
+    }
 }
