b9ce79b0d153d478032da2c5f4d0985556ae835d
Alifyz <alifyz@outlook.com>
2017-11-10 22:14:01 -0200

    - Implementinng the Building Blocks of the Custom News Adapter

--- Android Basics/NewsApp/app/src/main/java/com/alifyz/newsapp/NewsAdapter.java
+++ Android Basics/NewsApp/app/src/main/java/com/alifyz/newsapp/NewsAdapter.java
@@ -1,8 +1,27 @@
 package com.alifyz.newsapp;
 
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+
+import java.util.List;
+
 /**
  * Created by Alifyz on 11/10/2017.
  */
 
-public class NewsAdapter {
+public class NewsAdapter extends ArrayAdapter<News> {
+
+    public NewsAdapter(Context context, List<News> objects) {
+        super(context, 0, objects);
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        return super.getView(position, convertView, parent);
+    }
 }
