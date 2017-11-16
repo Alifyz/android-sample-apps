b73acf029f4fc090f51ac96d202a0eed81c7a9f4
Alifyz <alifyz@outlook.com>
2017-11-16 00:02:38 -0200

    Database Schema and Contract Class

--- Android Basics/HabitTracker/app/src/main/java/com/alifyz/habittracker/Database/HabitDbHelper.java
+++ Android Basics/HabitTracker/app/src/main/java/com/alifyz/habittracker/Database/HabitDbHelper.java
@@ -3,6 +3,7 @@ package com.alifyz.habittracker.Database;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import com.alifyz.habittracker.Database.HabitContract.HabitDataEntry;
 
 /**
  * Created by Alifyz on 11/15/2017.
@@ -19,7 +20,14 @@ public class HabitDbHelper extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase database) {
+        String CREATE_TABLE = "CREATE TABLE " + HabitDataEntry.TAB_NAME + " ("
+                +HabitDataEntry._ID + "INTEGER PRIMARY KEY AUTO INCREMENT, "
+                +HabitDataEntry.COLUMN_NAME + "TEXT NOT NULL, "
+                +HabitDataEntry.COLUMN_GENDER + "INTEGER NOT NULL, "
+                +HabitDataEntry.COLUMN_HEIGHT + "INTEGER NOT NULL, "
+                +HabitDataEntry.COLUMN_WEIGHT + "INTEGER NOT NULL);";
 
+        database.execSQL(CREATE_TABLE);
 
     }
 
