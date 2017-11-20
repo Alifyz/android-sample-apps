d00e5fcef48e56460f0f1ff24ac520cd8852963c
Alifyz Pires <alifyz@outlook.com>
2017-11-20 01:36:28 -0200

    Add: Adding constants to describe the basic structure of the Database
    Creating a static inner class that represents the basic structure of the database.

--- Android Basics/InventoryApp/app/src/main/java/com/alifyz/inventoryapp/Database/ProductDb.java
+++ Android Basics/InventoryApp/app/src/main/java/com/alifyz/inventoryapp/Database/ProductDb.java
@@ -1,8 +1,26 @@
 package com.alifyz.inventoryapp.Database;
 
+import android.provider.BaseColumns;
+
 /**
  * Created by Alifyz on 11/19/2017.
  */
 
 public class ProductDb {
+
+    private ProductDb() {}
+
+    public static class ProductEntry implements BaseColumns {
+
+        public static final String TABLE_NAME = "products";
+        public static final String _ID = BaseColumns._ID;
+        public static final String COLUMN_NAME = "name";
+        public static final String COLUMN_PRICE = "price";
+        public static final String COLUMN_QUANTITY = "quantity";
+        public static final String COLUMN_IMAGE = "image";
+        public static final String COLUMN_SALES = "sales";
+        public static final String COLUMN_SUPLIER_NAME = "suplier";
+        public static final String COLUMN_SUPLIER_CONTACT = "contact";
+
+    }
 }
