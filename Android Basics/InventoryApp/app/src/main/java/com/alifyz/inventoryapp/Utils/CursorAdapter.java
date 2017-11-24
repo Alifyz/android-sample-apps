f22218472419f5cf875cded1e22d4a3221dd1b54
Alifyz <alifyz@outlook.com>
2017-11-24 01:22:02 -0200

    Add: Basic inheritance from the CursorAdapter

--- Android Basics/InventoryApp/app/src/main/java/com/alifyz/inventoryapp/Utils/CursorAdapter.java
+++ Android Basics/InventoryApp/app/src/main/java/com/alifyz/inventoryapp/Utils/CursorAdapter.java
@@ -1,8 +1,28 @@
 package com.alifyz.inventoryapp.Utils;
 
+import android.content.Context;
+import android.database.Cursor;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
 /**
  * Created by alifyzpires on 2017-11-21.
  */
 
-public class CursorAdapter {
+public class CursorAdapter extends android.widget.CursorAdapter{
+
+    public CursorAdapter(Context context, Cursor c) {
+        super(context, c);
+    }
+
+    @Override
+    public View newView(Context context, Cursor cursor, ViewGroup viewGroup) {
+        return null;
+    }
+
+    @Override
+    public void bindView(View view, Context context, Cursor cursor) {
+
+    }
 }
