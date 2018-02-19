6e6b0a3d94aaad1db942b1dbf09825ae819fdbc3
Alifyz Pires <alifyz@outlook.com>
2018-02-18 22:48:02 -0300

    Add: Minimal ExoPlayer Functionality.
    Simple, but yet functional exoPlayer View capable of loading videos from a URL Address into the activity.

--- Android Developer/BakingApp/app/src/main/java/com/example/alify/bakingapp/StepsActivity.java
+++ Android Developer/BakingApp/app/src/main/java/com/example/alify/bakingapp/StepsActivity.java
@@ -1,12 +1,36 @@
 package com.example.alify.bakingapp;
 
 import android.content.Intent;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+
+import com.google.android.exoplayer2.DefaultLoadControl;
+import com.google.android.exoplayer2.ExoPlayerFactory;
+import com.google.android.exoplayer2.LoadControl;
+import com.google.android.exoplayer2.SimpleExoPlayer;
+import com.google.android.exoplayer2.extractor.DefaultExtractorsFactory;
+import com.google.android.exoplayer2.extractor.ExtractorsFactory;
+import com.google.android.exoplayer2.source.ExtractorMediaSource;
+import com.google.android.exoplayer2.source.MediaSource;
+import com.google.android.exoplayer2.source.dash.DashChunkSource;
+import com.google.android.exoplayer2.source.dash.DashMediaSource;
+import com.google.android.exoplayer2.source.dash.DefaultDashChunkSource;
+import com.google.android.exoplayer2.trackselection.AdaptiveTrackSelection;
+import com.google.android.exoplayer2.trackselection.DefaultTrackSelector;
+import com.google.android.exoplayer2.trackselection.TrackSelection;
+import com.google.android.exoplayer2.trackselection.TrackSelector;
+import com.google.android.exoplayer2.ui.SimpleExoPlayerView;
+import com.google.android.exoplayer2.upstream.DataSource;
+import com.google.android.exoplayer2.upstream.DefaultBandwidthMeter;
+import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory;
+import com.google.android.exoplayer2.upstream.DefaultHttpDataSourceFactory;
+import com.google.android.exoplayer2.util.Util;
 
 import java.util.HashMap;
 
@@ -18,12 +42,22 @@ public class StepsActivity extends AppCompatActivity {
     private int mPosition = 0;
     private int mMaxItem;
     private HashMap<String, String> mInformation;
+    private String mVideoUrl;
+
+    private static final DefaultBandwidthMeter BANDWIDTH_METER =
+            new DefaultBandwidthMeter();
 
 
-    @BindView(R.id.btn_previous) Button btnPrevious;
-    @BindView(R.id.btn_next)     Button btnNext;
+    @BindView(R.id.btn_previous)
+    Button btnPrevious;
+    @BindView(R.id.btn_next)
+    Button btnNext;
     @BindView(R.id.tv_step_description)
     TextView mTextRecipeDescription;
+    @BindView(R.id.exoplayer_view)
+    SimpleExoPlayerView mPlayerView;
+    SimpleExoPlayer mSimpleExoPlayer;
+
 
 
     @Override
@@ -39,12 +73,12 @@ public class StepsActivity extends AppCompatActivity {
         mInformation = (HashMap<String, String>) mData.getSerializableExtra("stepsInfo");
         mMaxItem = getMaxSize(mInformation);
 
+        mVideoUrl = mInformation.get("videoURL_"+ mPosition);
+        initPlayer(mVideoUrl);
+
         mTextRecipeDescription.setText(mInformation.get("description_" + mPosition)); //Initial value
+        mPlayerView.setDefaultArtwork(BitmapFactory.decodeResource(getResources(), R.drawable.coffee));
 
-
-        //TODO - 1 Unpack the Information
-        //TODO - 2 Assign the respective value into its views
-        //TODO - 3 Create a Dynamic system to change the behavior of each one.
 
         btnNext.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -71,8 +105,11 @@ public class StepsActivity extends AppCompatActivity {
             mPosition++;
         }
         if(mPosition < mMaxItem) {
+            mSimpleExoPlayer = null;
             setTitle("Step - " + mPosition);
             String recipeDescription = mInformation.get("description_" + mPosition);
+            mVideoUrl = mInformation.get("videoURL_"+ mPosition);
+            initPlayer(mVideoUrl);
             mTextRecipeDescription.setText(recipeDescription);
         }
     }
@@ -82,11 +119,54 @@ public class StepsActivity extends AppCompatActivity {
             mPosition--;
         }
         if(mPosition >= 0) {
+            mSimpleExoPlayer = null;
             setTitle("Step - " + mPosition);
             String recipeDescription = mInformation.get("description_" + mPosition);
+            mVideoUrl = mInformation.get("videoURL_"+ mPosition);
+            initPlayer(mVideoUrl);
             mTextRecipeDescription.setText(recipeDescription);
         }
     }
 
 
+    private void initPlayer(String mVideoUrl) {
+        if(mSimpleExoPlayer == null) {
+
+            TrackSelection.Factory mAdaptativeTrackSelector = new AdaptiveTrackSelection.Factory(BANDWIDTH_METER);
+            TrackSelector mTrackSelector = new DefaultTrackSelector(mAdaptativeTrackSelector);
+
+            LoadControl mLoadControl = new DefaultLoadControl();
+            mSimpleExoPlayer = ExoPlayerFactory.newSimpleInstance(this, mTrackSelector, mLoadControl);
+            mPlayerView.setPlayer(mSimpleExoPlayer);
+
+            MediaSource mMediaSource = buildMediaSource(Uri.parse(mVideoUrl));
+            mSimpleExoPlayer.prepare(mMediaSource);
+            mSimpleExoPlayer.setPlayWhenReady(true);
+        }
+    }
+
+
+    private MediaSource buildMediaSource(Uri uri) {
+
+        DataSource.Factory dataSourceFactory =
+                new DefaultDataSourceFactory(this, Util.getUserAgent(this, "BakingApp"));
+
+        ExtractorsFactory extractorsFactory = new DefaultExtractorsFactory();
+
+        MediaSource videoSource = new ExtractorMediaSource(uri,
+                dataSourceFactory, extractorsFactory, null, null);
+
+        return videoSource;
+    }
+
+    private void releasePlayer() {
+        mSimpleExoPlayer.release();
+        mSimpleExoPlayer = null;
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        releasePlayer();
+    }
 }
