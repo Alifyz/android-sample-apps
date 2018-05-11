7919235275330de30f527945d17683fc21b61c1b
Alifyz Pires <alifyz@outlook.com>
2018-05-11 00:43:53 -0300

    add: startup project
    initial version of the mapsMania App.

---
+++ MapsMania/app/src/main/java/alifyz/com/mapsmania/MapsActivity.java
@@ -0,0 +1,13 @@
+package alifyz.com.mapsmania;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MapsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+    }
+}
