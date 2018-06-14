3e21f6df425ad0de1ff67c0c6e70aa166bca0110
Alifyz Pires <alifyz@outlook.com>
2018-06-14 00:48:20 -0300

    add: Google Code Lab - Room Android Architecture Components.

---
+++ RoomCodeLab/app/src/main/java/alifyz/com/roomsample/MainActivity.java
@@ -0,0 +1,101 @@
+package alifyz.com.roomsample;
+
+import android.arch.lifecycle.Observer;
+import android.arch.lifecycle.ViewModel;
+import android.arch.lifecycle.ViewModelProvider;
+import android.arch.lifecycle.ViewModelProviders;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    public static final int NEW_WORD_ACTIVITY_REQUEST_CODE = 1;
+    RecyclerView mRecyclerView;
+    WordAdapter mAdapter;
+    LinearLayoutManager mLayout;
+
+    private WordViewModel mViewModel;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        mViewModel = ViewModelProviders.of(this).get(WordViewModel.class);
+        mViewModel.getmAllWords().observe(this, new Observer<List<WordEntity>>() {
+            @Override
+            public void onChanged(@Nullable List<WordEntity> wordEntities) {
+                mAdapter.setWords(wordEntities);
+            }
+        });
+
+        mRecyclerView = findViewById(R.id.recyclerview);
+        mLayout = new LinearLayoutManager(this);
+        mAdapter = new WordAdapter(this);
+
+        mRecyclerView.setLayoutManager(mLayout);
+        mRecyclerView.setAdapter(mAdapter);
+
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, NewWord.class);
+                startActivityForResult(intent, NEW_WORD_ACTIVITY_REQUEST_CODE);
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == NEW_WORD_ACTIVITY_REQUEST_CODE && resultCode == RESULT_OK) {
+            WordEntity word = new WordEntity(data.getStringExtra(NewWord.EXTRA_REPLY));
+            mViewModel.insert(word);
+        } else {
+            Toast.makeText(
+                    getApplicationContext(),
+                    R.string.empty_not_saved,
+                    Toast.LENGTH_LONG).show();
+        }
+    }
+}
