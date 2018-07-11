c5840227bbf38ef777e51b202684f1884d2ddb25
Alifyz Pires <alifyz@outlook.com>
2018-07-11 15:31:53 -0300

    del: deleting the Network interface for retrofit & adding logging into the gradle dependencies

---
+++ PopSeries/app/src/main/java/alifyz/com/popseries/network/NetworkApi.kt
@@ -0,0 +1,10 @@
+package alifyz.com.popseries.network
+
+import alifyz.com.popseries.model.TvSeries
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface NetworkApi {
+
+}
