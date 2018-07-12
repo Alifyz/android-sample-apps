b5dff0c52513b1da24704c48888ded5a9ebadbd8
Alifyz Pires <alifyz@outlook.com>
2018-07-11 22:56:14 -0300

    del: deleting the Network dependencies and

--- PopSeries/app/src/main/java/alifyz/com/popseries/MainActivity.kt
+++ PopSeries/app/src/main/java/alifyz/com/popseries/MainActivity.kt
@@ -1,12 +1,17 @@
 package alifyz.com.popseries
 
+import alifyz.com.popseries.model.Series
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
+
 class MainActivity : AppCompatActivity() {
+
+    val series : Series? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
 }
--- PopSeries/app/src/main/java/alifyz/com/popseries/network/NetworkApi.kt
+++ PopSeries/app/src/main/java/alifyz/com/popseries/network/NetworkApi.kt
@@ -1,10 +1,15 @@
 package alifyz.com.popseries.network
 
-import alifyz.com.popseries.model.TvSeries
+import alifyz.com.popseries.model.Series
 import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface NetworkApi {
 
+    @GET("discover/tv")
+    fun getSeries(
+            @Query("api_key") api_key : String,
+            @Query("language") language : String,
+            @Query("page") page : String) : Call<Series>
 }
