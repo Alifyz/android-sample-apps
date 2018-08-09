16726e11b9aafa8808a37022d35e8f8f5035573b
alifyz <alifyz@outlook.com>
2018-08-09 00:47:01 -0300

    add: hello world RxJava

--- RxJavaSample/app/src/main/java/alifyz/com/rxjavasample/MainActivity.java
+++ RxJavaSample/app/src/main/java/alifyz/com/rxjavasample/MainActivity.java
@@ -2,12 +2,32 @@ package alifyz.com.rxjavasample;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import io.reactivex.Observable;
+import io.reactivex.functions.Consumer;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.hello_world)
+    TextView mHelloWorld;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        Observable.just("Hello RxJava")
+                .subscribe(new Consumer<String>() {
+                    @Override
+                    public void accept(String s) throws Exception {
+                        mHelloWorld.setText(s);
+                    }
+                });
     }
 }
