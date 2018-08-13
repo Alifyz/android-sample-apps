3c932df029a8e65463c01c371269cac0abc1076b
alifyz <alifyz@outlook.com>
2018-08-13 16:38:12 -0300

    add: simple implementation of a custom view.

--- CustomView/app/src/main/java/alifyz/com/customviewsample/SuperText/SuperText.java
+++ CustomView/app/src/main/java/alifyz/com/customviewsample/SuperText/SuperText.java
@@ -1,12 +1,26 @@
 package alifyz.com.customviewsample.SuperText;
 
 import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Paint;
 import android.support.v7.widget.AppCompatTextView;
 
 public class SuperText extends AppCompatTextView {
 
+    private Paint backgroundColor;
+
     public SuperText(Context context) {
         super(context);
+
+        backgroundColor = new Paint();
+        backgroundColor.setColor(0xffff0000);
+        backgroundColor.setStyle(Paint.Style.FILL);
+
     }
 
+    @Override
+    protected void onDraw(Canvas canvas) {
+        canvas.drawRect(0,0, getWidth(), getHeight(), backgroundColor);
+        super.onDraw(canvas);
+    }
 }
