02061c9ba1018df10a02d066304932853e514a5c
Alifyz Pires <alify.pires@maximasistemas.com.br>
2018-09-24 08:51:00 -0300

    refactor: Database example from the codelabs.

--- RoomWithLiveData/app/src/main/java/com/alifyz/roomwithlivedata/repository/DesertRepository.kt
+++ RoomWithLiveData/app/src/main/java/com/alifyz/roomwithlivedata/repository/DesertRepository.kt
@@ -1,12 +1,10 @@
 package com.alifyz.roomwithlivedata.repository
 
-import android.app.Application
+
 import android.arch.lifecycle.LiveData
 import android.support.annotation.WorkerThread
-import com.alifyz.roomwithlivedata.database.files.AppDatabase
 import com.alifyz.roomwithlivedata.database.files.dao.DesertDao
 import com.alifyz.roomwithlivedata.database.files.entity.DesertEntity
-import org.jetbrains.anko.doAsync
 
 class DesertRepository(private val dao : DesertDao) {
 
