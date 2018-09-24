74ee0e9534de91babfa4ed72fe245df9df37e8ad
Alifyz Pires <alify.pires@maximasistemas.com.br>
2018-09-24 11:03:47 -0300

    add: Adapter.

--- RoomWithLiveData/app/src/main/java/com/alifyz/roomwithlivedata/MainActivity.kt
+++ RoomWithLiveData/app/src/main/java/com/alifyz/roomwithlivedata/MainActivity.kt
@@ -3,17 +3,25 @@ package com.alifyz.roomwithlivedata
 import android.os.Bundle
 import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.LinearLayoutManager
 import android.view.Menu
 import android.view.MenuItem
+import com.alifyz.roomwithlivedata.recycler.DesertAdapter
 
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.content_main.*
 
 class MainActivity : AppCompatActivity() {
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
+
+        val adapter = DesertAdapter(this)
+        recyclerview.adapter = adapter
+        recyclerview.layoutManager = LinearLayoutManager(this)
 
         fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
