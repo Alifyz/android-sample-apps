c2647baa94876fb16f6cd9575eb5a3782cd39fff
Alifyz Pires <alifyz@outlook.com>
2018-11-08 22:48:33 -0200

    refactor: implementing MVP Arch
    Implementing the MVP architecture, and refactoring a few classes.

--- PopSeries/app/src/main/java/alifyz/com/popseries/ui/TrendingSeriesFragment.kt
+++ PopSeries/app/src/main/java/alifyz/com/popseries/ui/TrendingSeriesFragment.kt
@@ -1,30 +1,22 @@
 package alifyz.com.popseries.ui
 
-import alifyz.com.popseries.BuildConfig
 import alifyz.com.popseries.R
 import alifyz.com.popseries.adapter.HomeSeriesAdapter
+import alifyz.com.popseries.arch.TrendingContract
+import alifyz.com.popseries.arch.TrendingPresenter
 import alifyz.com.popseries.model.SeriesModel
-import alifyz.com.popseries.network.RetrofitHelper
-import alifyz.com.popseries.network.SeriesEndpoint
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.support.v7.widget.GridLayoutManager
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import kotlinx.android.synthetic.main.fragment_top.*
-import retrofit2.Call
-import retrofit2.Callback
 import retrofit2.Response
 
-class TrendingSeriesFragment : Fragment() {
+class TrendingSeriesFragment : Fragment(), TrendingContract.View {
 
-    val gridLayout = GridLayoutManager(
-            context,
-            2,
-            GridLayoutManager.VERTICAL,
-            false)
+    override lateinit var presenter: TrendingPresenter
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_top, container, false)
@@ -32,33 +24,40 @@ class TrendingSeriesFragment : Fragment() {
 
     override fun onResume() {
         super.onResume()
-        progress.visibility = View.VISIBLE
-        recyclerview_top.visibility = View.GONE
-
-        val retrofit = RetrofitHelper.getInstance()
-
-        val endpoint = retrofit.create(SeriesEndpoint::class.java)
-        val call = endpoint.getTopSeries(BuildConfig.API_KEY)
-
-        call.enqueue(object : Callback<SeriesModel> {
-            override fun onResponse(call: Call<SeriesModel>?, response: Response<SeriesModel>?) {
-                Log.d("RetrofitHelper: ", "Success")
-                storeSeries(response)
-            }
-
-            override fun onFailure(call: Call<SeriesModel>?, t: Throwable?) {
-                Log.d("RetrofitHelper: ", "Failed")
-                TODO("not implemented")
-            }
-        })
+        presenter = TrendingPresenter(this)
+        presenter.start()
     }
 
-    fun storeSeries(response: Response<SeriesModel>?) {
-        val response_body = response?.body()
-        progress.visibility = View.GONE
-        recyclerview_top.visibility = View.VISIBLE
+    override fun setLoadingIndicator(active: Boolean) {
+        if(active) {
+            progress.visibility = View.VISIBLE
+            recyclerview_top.visibility = View.GONE
+        } else {
+            progress.visibility = View.GONE
+            recyclerview_top.visibility = View.VISIBLE
+        }
+    }
 
+    override fun showEmptyContent() {
+        TODO("not implemented")
+    }
+
+    override fun showOffline() {
+        TODO("not implemented")
+    }
+
+    override fun showSavedAlert() {
+        TODO("not implemented")
+    }
+
+    override fun setAdapter(response: Response<SeriesModel>?) {
+        val gridLayout = GridLayoutManager(context, 2, GridLayoutManager.VERTICAL, false)
         recyclerview_top.layoutManager = gridLayout
-        recyclerview_top.adapter = HomeSeriesAdapter(context!!, response_body!!)
+        recyclerview_top.adapter = HomeSeriesAdapter(context!!, response!!.body()!!)
     }
+
+    override fun showErrorScreen() {
+        TODO("not implemented")
+    }
+
 }
