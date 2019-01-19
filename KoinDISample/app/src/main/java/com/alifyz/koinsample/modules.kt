9d4963bab899c8ab26b02e6996efa26052e7457f
alifyz <double3neck>
2019-01-19 16:00:38 -0200

    add: new dependency injection example

--- KoinDISample/app/src/main/java/com/alifyz/koinsample/modules.kt
+++ KoinDISample/app/src/main/java/com/alifyz/koinsample/modules.kt
@@ -1,9 +1,11 @@
 package com.alifyz.koinsample
 
+import com.alifyz.koinsample.data.DataRepositoryImpl
 import com.google.gson.Gson
 import org.koin.dsl.module.module
 
 val applicationModule = module {
     single { Gson() }
     factory { CurrencyAdapter() }
+    factory { DataRepositoryImpl() }
 }
