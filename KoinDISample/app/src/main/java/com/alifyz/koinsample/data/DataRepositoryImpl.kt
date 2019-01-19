aa9faf4e6cd54aa86b5adb23434a6cf4a369ec41
alifyz <double3neck>
2019-01-19 15:30:46 -0200

    add: new dependency injection example

---
+++ KoinDISample/app/src/main/java/com/alifyz/koinsample/data/DataRepositoryImpl.kt
@@ -0,0 +1,11 @@
+package com.alifyz.koinsample.data
+
+import com.alifyz.koinsample.model.Currency
+import com.google.gson.Gson
+
+class DataRepositoryImpl(val gson : Gson) {
+
+    fun getCurrencies(jsonString : String) : List<Currency> {
+        return gson.fromJson(jsonString, Array<Currency>::class.java).toList()
+    }
+}
---
+++ KoinDISample/app/src/main/java/com/alifyz/koinsample/model/Currency.kt
@@ -0,0 +1,6 @@
+package com.alifyz.koinsample.model
+
+data class Currency(val id: Int,
+                    val name : String,
+                    val symbol: String,
+                    val slug : String)
