1f24f7a9bbcd6baad7b00e39cc1f544430949690
alifyz <double3neck>
2019-01-28 21:54:44 -0200

    add: Anko Demo

--- AnkoDemo/app/src/main/java/com/alifyz/ankodemo/MainActivity.kt
+++ AnkoDemo/app/src/main/java/com/alifyz/ankodemo/MainActivity.kt
@@ -1,12 +1,36 @@
 package com.alifyz.ankodemo
 
-import android.support.v7.app.AppCompatActivity
+import android.content.DialogInterface
 import android.os.Bundle
+import android.support.v7.app.AlertDialog
+import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+import org.jetbrains.anko.toast
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btn_toast.setOnClickListener {
+            toast("Mensagem exemplo")
+        }
+
+        btn_dialog.setOnClickListener {
+
+            val alertBuilder = AlertDialog.Builder(this)
+            alertBuilder.setTitle("Atenção!")
+            alertBuilder.setMessage("Mensagem de Exemplo de Alerta!!")
+            alertBuilder.setPositiveButton("OK", DialogInterface.OnClickListener { _, which ->
+                toast("Ação Positiva")
+            })
+            alertBuilder.setNegativeButton("Cancelar", DialogInterface.OnClickListener{_, which ->
+                toast("Ação Negativa")
+            })
+
+            val alert = alertBuilder.create()
+            alert.show()
+        }
     }
 }
