fb28c400e75803eb5db6f280de4a3df693c73daa
alifyz <double3neck>
2019-01-28 22:54:22 -0200

    add: Anko Demo

--- AnkoDemo/app/src/main/java/com/alifyz/ankodemo/MainActivity.kt
+++ AnkoDemo/app/src/main/java/com/alifyz/ankodemo/MainActivity.kt
@@ -1,7 +1,5 @@
 package com.alifyz.ankodemo
 
-import android.content.Intent
-import android.net.Uri
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
