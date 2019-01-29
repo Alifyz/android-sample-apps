557c46c2c100e746832f9812ed6bf9e5dac9f692
alifyz <double3neck>
2019-01-28 22:53:29 -0200

    add: Anko Demo

--- AnkoDemo/app/src/main/java/com/alifyz/ankodemo/MainActivity.kt
+++ AnkoDemo/app/src/main/java/com/alifyz/ankodemo/MainActivity.kt
@@ -1,11 +1,12 @@
 package com.alifyz.ankodemo
 
-import android.content.DialogInterface
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
-import android.support.v7.app.AlertDialog
 import android.support.v7.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
-import org.jetbrains.anko.toast
+import org.jetbrains.anko.*
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -18,19 +19,35 @@ class MainActivity : AppCompatActivity() {
         }
 
         btn_dialog.setOnClickListener {
+            alert("Mensagem de Alerta") {
+                titleResource = R.string.title
+                yesButton { toast("Mensagem Positiva")}
+                noButton { toast("Mensagem Negativa") }
+            }.show()
+        }
 
-            val alertBuilder = AlertDialog.Builder(this)
-            alertBuilder.setTitle("Atenção!")
-            alertBuilder.setMessage("Mensagem de Exemplo de Alerta!!")
-            alertBuilder.setPositiveButton("OK", DialogInterface.OnClickListener { _, which ->
-                toast("Ação Positiva")
-            })
-            alertBuilder.setNegativeButton("Cancelar", DialogInterface.OnClickListener{_, which ->
-                toast("Ação Negativa")
-            })
+        btn_activity.setOnClickListener {
+            startActivity(intentFor<OtherActivity>("id" to 10).clearTop())
+        }
 
-            val alert = alertBuilder.create()
-            alert.show()
+        btn_call.setOnClickListener {
+           //Realizar uma Chamada
+            makeCall("+666")
+        }
+
+        btn_sms.setOnClickListener {
+            //Enviar um SMS
+           sendSMS("+666", "Hello, it's me! :)")
+        }
+
+        btn_browse.setOnClickListener {
+            //Navegar pela Internet
+            browse("http://www.alifyz.com")
+        }
+
+        btn_email.setOnClickListener {
+            //Enviar um E-mail
+            email("alifyz@gmail.com","Assunto do E-mail", "Olá, texto padrão de E-mail")
         }
     }
 }
