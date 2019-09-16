222b4b3c9dae538e702d45494161fad1636b9ed6
Alifyz <alifyz@outlook.com>
2019-09-16 18:04:49 -0300

    exoplayer example

--- ExoplayerWithAds/app/src/main/java/com/alifyz/exoplayerwithads/MainActivity.kt
+++ ExoplayerWithAds/app/src/main/java/com/alifyz/exoplayerwithads/MainActivity.kt
@@ -1,12 +1,48 @@
 package com.alifyz.exoplayerwithads
 
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.exoplayer2.DefaultLoadControl
+import com.google.android.exoplayer2.DefaultRenderersFactory
+import com.google.android.exoplayer2.ExoPlayerFactory
+import com.google.android.exoplayer2.ext.ima.ImaAdsLoader
+import com.google.android.exoplayer2.extractor.DefaultExtractorsFactory
+import com.google.android.exoplayer2.source.MediaSource
+import com.google.android.exoplayer2.source.ProgressiveMediaSource
+import com.google.android.exoplayer2.source.ads.AdsMediaSource
+import com.google.android.exoplayer2.trackselection.DefaultTrackSelector
+import com.google.android.exoplayer2.upstream.DefaultHttpDataSource
+import com.google.android.exoplayer2.upstream.DefaultHttpDataSourceFactory
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var imaAdsLoader : ImaAdsLoader
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val uri = Uri.parse(getString(R.string.ad_tag_url))
+        imaAdsLoader = ImaAdsLoader(this, uri)
     }
+
+
+    override fun onStart() {
+        super.onStart()
+        val player = ExoPlayerFactory.newSimpleInstance(
+            this,
+            DefaultRenderersFactory(this),
+            DefaultTrackSelector(),
+            DefaultLoadControl())
+        playerView.player = player
+
+        player.playWhenReady = true
+
+
+
+    }
+
+
 }
